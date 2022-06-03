db857812f5da1db7b7ddcf2796186c3ed9c585c9
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-03 11:27:34 -0300

    estatisticas funcionais

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
 const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
+console.log(estatistica)
+
+const pecas = {
+  "bracos": {
+      "forca": 29,
+      "poder": 35,
+      "energia": -21,
+      "velocidade": -5
+  },
+
+  "blindagem": {
+      "forca": 41,
+      "poder": 20,
+      "energia": 0,
+      "velocidade": -20
+  },
+  "nucleos":{
+      "forca": 0,
+      "poder": 7,
+      "energia": 48,
+      "velocidade": -24
+  },
+  "pernas":{
+      "forca": 27,
+      "poder": 21,
+      "energia": -32,
+      "velocidade": 42
+  },
+  "foguetes":{
+      "forca": 0,
+      "poder": 28,
+      "energia": 0,
+      "velocidade": -2
+  }
+}
+
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)})
+    manipulaDados(evento.target.dataset.controle, evento.target.  parentNode)
+
+   atualizaPeca(evento.target.dataset.peca);
+  
+  })
+
 });
 
 function manipulaDados(operacao, controle){
@@ -15,5 +57,13 @@ function manipulaDados(operacao, controle){
     peca.value = parseInt(peca.value) + 1 
   }
 
+}
+
+function atualizaPeca(peca){
+
+  estatistica.forEach((elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+  });
+  console.log (this)
 }
       
